40db3b16c1604e9659d32328e3c15c3299956d9a
Avext <80375626+Avext@users.noreply.github.com>
2022-07-24 01:11:01 -0300

    Update cadastro.py
    Versão do código feita por Thiago em uma branch separada.

--- cadastro.py
+++ cadastro.py
@@ -1,22 +1,10 @@
 import datetime
 import pandas as pd
 
-class Verify:
-
-     def __init__(self):
-        pass
-     def verifica_textos(self,resposta):
-        self.resposta = resposta
-        quest = ['a','b','c']
-        if resposta not in quest:
-            print('Digite uma opção válida')
-            return False
-        else:
-            return resposta
-
+#dicionários que armazenam os dados
 data = {
-    'city':[],
-    'state':[],
+    'city':'',
+    'state':'',
     'ages':[],
     'genres':[],
     'answers1':[],
@@ -26,73 +14,83 @@ data = {
     'dateAnswer':[],
 }
 
-# colunas = dados acima
-# perguntas = 'answer': [a ou b ou c]
+class Pesquisa:
+    def __init__(self, estado, cidade):
+        self.estado = estado
+        self.cidade = cidade
+        data['state'] = self.estado
+        data['city'] = self.cidade
 
+    def entrevistados (self, idade: int, genero):
+        self.idade = idade
+        self.genero = genero
+        data['ages'].append(self.idade)
+        data['genres'].append(self.genero)
 
-def perguntas():
-    
-    state = input('\nEm qual estado você mora ? ')
-    city = input('\nQual a sua cidade ?  ')
-    age = int(input('\nQual a sua idade ? ')) 
-    if age < 1:
-        print('Por favor, digite um valor válido para a idade.')
-        perguntas()
+    def fazerPergunta1(self, r1: int):
+        self.r1 = r1
+        if self.r1 != 1 and self.r1 != 2 and self.r1 != 3:
+            data['answers1'].append(3)
+
+        else:
+            data['answers1'].append(self.r1) 
+
+    def fazerPergunta2(self, r2: int):
+        self.r2 = r2
+        if self.r2 != 1 and self.r2 != 2 and self.r2 != 3:
+            data['answers2'].append(3)
+
+        else:
+            data['answers2'].append(self.r2) 
+
+    def fazerPergunta3(self, r3: int):
+        self.r3 = r3
+        if self.r3 != 1 and self.r3 != 2 and self.r3 != 3:
+            data['answers3'].append(3)
+
+        else:
+            data['answers3'].append(self.r3) 
+
+    def fazerPergunta4(self, r4: int):
+        self.r4 = r4
+        if self.r4 != 1 and self.r4 != 2 and self.r4 != 3:
+            data['answers4'].append(3)
+
+        else:
+            data['answers4'].append(self.r4) 
+
+    def registroDataHora(self):
+        data['dateAnswer'].append(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+
+    def exportCsv(self):
+        #Bloco para salvar csv
+        sheet = pd.DataFrame(data)
+        sheet.to_csv('Pesquisa_'+data['state']+'-'+data['city']+'.csv', encoding = 'utf-8')
+            
+
+novaPesquisa = Pesquisa(input('\nDigite a sigla do estado onde esta pesquisa está sendo realizada e tecle enter... ').upper(), input('\nDigite a cidade onde esta pesquisa está sendo realizada e tecle enter... '))
+
+insert = True
+while insert:
+
+    age = int(input('\nDigite a idade da pessoa entrevistada e tecle enter... '))
+        
+    if age > 0:
+        
+        novaPesquisa.entrevistados(age, input('\nDigite o gênero da pessoa entrevistada e tecle enter... ').lower())
+
+        novaPesquisa.fazerPergunta1(input('\nEsta é a pergunta 1 \nSim(1)\nNão(2)\nNão sei responder(3)'))
+        novaPesquisa.fazerPergunta2(input('\nEsta é a pergunta 2 \nSim(1)\nNão(2)\nNão sei responder(3)'))
+        novaPesquisa.fazerPergunta3(input('\nEsta é a pergunta 3 \nSim(1)\nNão(2)\nNão sei responder(3)'))
+        novaPesquisa.fazerPergunta4(input('\nEsta é a pergunta 4 \nSim(1)\nNão(2)\nNão sei responder(3)'))
+
+        novaPesquisa.registroDataHora()
+
+    elif age == 0:
+        insert = False
+
     else:
-        data['state'].append(state)
-        data['city'].append(city)
-        data['ages'].append(age)
-        perguntas2()
+        print('Entrada inválida!')
 
-def perguntas2():
-    genre = input('\nQual o seu gênero [M/F] ? ')
-    if genre == 'M'.lower() or 'F'.lower():
-        data['genres'].append(genre)
-        perguntas3()
-    else:
-        print("Digite apenas os caracteres correspondentes!")
-        perguntas2()
-
-def perguntas3():
-    questionText1 = str(input('\nVocê já concluiu o Ensino Médio ?\na) Sim\nb) Não\nc)Não Sei\n Digite a opção correspondente:  '))
-    questionText2 = str(input('\nA média salarial da sua residência ultrapassa os R$ 2.000 mensais ?\na) Sim\nb) Não\nc)Não Sei\n Digite a opção correspondente: '))
-    questionText3 = str(input('\nDurante o perído da pandemia, você notou se alguma pessoa próxima desenvolver\n ou agravar a depressão ou alguma doença semelhante ?\na) Sim\nb) Não\nc)Não Sei\n Digite a opção correspondente:  '))
-    questionText4 = str(input('\nVocê consideraria buscar apoio psicológico para ajudar essa ou essas pessoas ?\na) Sim\nb) Não\nc)Não Sei\n Digite a opção correspondente:  '))
-    # question1 retorna apenas a letra da resposta
-    if Verify.verifica_textos(questionText1) == False:
-        return questionText1
-    verfalse(questionText2)
-    verfalse(questionText3)
-    verfalse(questionText4)
-
-    data['answers1'].append(questionText1)
-    data['answers2'].append(questionText2)
-    data['answers3'].append(questionText3)
-    data['answers4'].append(questionText4)
-    data['dateAnswer'].append(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
-    print('\nO Instituto agradece a participção de todos!')
-
-def verfalse(question):
-    quest = ['a','b','c']
-    if question not in quest:
-        print('Digite uma opção válida')
-        return False   
-    else:
-        return question
-
-#armazena o número de entrevistados (opcional)
-numberAnswers = len(data['ages'])   
-
-perguntas()
-
-#Bloco para salvar csv
-sheet = pd.DataFrame.from_dict(data,orient='index')
-print(sheet)
-# sheet.to_csv(sheet, encoding = 'utf-8')
-
-
-
-#print(data,numberAnswers)
-
-#O QUE FALTA:
-#orientação à objetos
+novaPesquisa.exportCsv()
+print('Obrigado pela participação')
